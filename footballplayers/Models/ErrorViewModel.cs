7e465c0b7e2bcc4668e9f555757ce33436f4849a
berkayinann <inan.berkay@outlook.com>
2023-12-20 23:24:23 +0300

    Proje dosyası ekle.

---
+++ footballplayers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace footballplayers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
